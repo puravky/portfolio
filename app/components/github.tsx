9c42731ddec0510807aff5c49430f5c0add2e53c
AvhiMaz <avhimazumder5@outlook.com>
2025-02-23 14:21:54 +0530

    feat: loading
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- app/components/github.tsx
+++ app/components/github.tsx
@@ -1,10 +1,5 @@
-import { Slide } from "./animations/Slide";
 import ContributionGraph from "./graph";
 
 export default function GithubCalendarComponent() {
-  return (
-    <Slide delay={0.18}>
-      <ContributionGraph />
-    </Slide>
-  );
+  return <ContributionGraph />;
 }
