1281549fb1b6c34c349ec3b96cfc1beda79cbccb
AvhiMaz <avhimazumder5@outlook.com>
2025-03-27 12:24:00 +0530

    feat: add video and update superteam page
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- app/experience/superteam/page.tsx
+++ app/experience/superteam/page.tsx
@@ -2,6 +2,8 @@
 
 import { Slide } from "@/components/animations/Slide";
 import Particles from "@/components/particle";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -13,13 +15,65 @@ export default function Page() {
 
         <section className="relative z-10">
           <div className="prose prose-neutral dark:prose-invert">
-            <p>superteam ~ member ~ grant recipient (2x)</p>
+            <p className="mb-8 text-2xl font-medium tracking-tight">
+              superteam ~ member ~ grant recipient (2x)
+            </p>
 
-            <div className="flex gap-3 mb-7 mt-7">hi hi</div>
-
-            {/* <div className="mt-9 w-full h-px bg-zinc-800"></div> */}
-            <p>hello</p>
-
+            <div>
+              <p>
+                got the opportunity to become a Superteam member and 2x grant
+                recipient. <br /> I’ve had the opportunity to work on innovative
+                projects in the Solana ecosystem, receiving a total of $3,000 in
+                grants for my contributions.
+                <br />
+              </p>
+              <video
+                autoPlay
+                loop
+                muted
+                width="720"
+                height="240"
+                preload="none"
+                className="rounded"
+              >
+                <source src="/projects/blink-arena.mp4" type="video/mp4" />
+              </video>
+              <p>
+                1st grant: Superteam x Dialect Blinks Received support from
+                Superteam x Dialect Blinks to build Blink Arena: a platform
+                where user can either host or join gaming tournament via solana
+                blinks. ~{" "}
+                <Link
+                  className=""
+                  href="https://www.avhimaz.in/projects/blink-arena"
+                >
+                  read more here
+                </Link>
+                <br />
+              </p>
+              <video
+                autoPlay
+                loop
+                muted
+                width="720"
+                height="240"
+                preload="none"
+                className="rounded"
+              >
+                <source src="/projects/nestfolio.mp4" type="video/mp4" />
+              </video>
+              <p>
+                2nd grant: Solana Foundation & CoinDCX Secured a second grant
+                from Solana Foundation & CoinDCX for Nestfolio: an ai-powered
+                dao governance. ~{" "}
+                <Link
+                  className=""
+                  href="https://www.avhimaz.in/projects/nestfolio"
+                >
+                  read more here
+                </Link>
+              </p>
+            </div>
           </div>
         </section>
       </div>
