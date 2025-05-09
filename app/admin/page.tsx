0181831b8666bc4cf9fc58ad54c5cc3ac6bcc6eb
AvhiMaz <avhimazumder5@outlook.com>
2025-05-09 17:39:07 +0530

    added ip prank
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

---
+++ app/admin/page.tsx
@@ -0,0 +1,16 @@
+import Admin from "@/components/admin";
+
+export const metadata = {
+  title: "admin",
+  description: "admin",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <Admin />
+    </div>
+  )
+}
+
+
