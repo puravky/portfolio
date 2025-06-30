1735e9bb413bf49f9d6203258f0aafca645f5db6
AvhiMaz <avhimazumder5@outlook.com>
2025-06-30 15:31:27 +0530

    update buttons animation

--- app/components/shared/year-button.tsx
+++ app/components/shared/year-button.tsx
@@ -12,9 +12,8 @@ export default function YearButton({
   return (
     <button
       onClick={onClick}
-      className={`rounded-lg text-center px-4 py-2 border border-transparent border-zinc-200  dark:border-zinc-700 text-sm font-medium cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-md ${
-        year === currentYear ? "  " : " "
-      }`}
+      className={`rounded-lg text-center px-4 py-2 border border-transparent border-zinc-200  dark:border-zinc-700 text-sm font-medium cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-md dark:hover:shadow-[0_4px_gray] ${year === currentYear ? "  " : " "
+        }`}
     >
       {year}
     </button>
